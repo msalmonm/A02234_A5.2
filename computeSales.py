1c9ec2f2513cfb5b90c77c3373581af9eeee0bf9
msalmonm <MMacal@hersheys.com>
2026-02-15 20:57:59 -0600

    refactor: replace nested loops with dictionary lookup (includes results)

--- computeSales.py
+++ computeSales.py
@@ -11,17 +11,19 @@ file_b = sys.argv[2]
 data_a = json.load(open(file_a))
 data_b = json.load(open(file_b))
 
+lookup_table = {}
+
+for element in data_a:
+    lookup_table[element["title"]] = element["price"]
+
 sum_value = 0
 
-for item_b in data_b:
-    match_found = False
+for sale_entry in data_b:
+    key_name = sale_entry["Product"]
 
-    for item_a in data_a:
-        if item_a["title"] == item_b["Product"]:
-            sum_value += item_a["price"] * item_b["Quantity"]
-            match_found = True
-
-    if not match_found:
-        print("Missing product:", item_b["Product"])
+    if key_name in lookup_table:
+        sum_value += lookup_table[key_name] * sale_entry["Quantity"]
+    else:
+        print("Missing product:", key_name)
 
 print("Result:", sum_value)
