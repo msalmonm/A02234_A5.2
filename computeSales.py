2bd811c96a42d8487d9cb0494d62fbf70ad42a34
msalmonm <MMacal@hersheys.com>
2026-02-15 20:55:01 -0600

    test: results v3

--- computeSales.py
+++ computeSales.py
@@ -14,8 +14,14 @@ data_b = json.load(open(file_b))
 sum_value = 0
 
 for item_b in data_b:
+    match_found = False
+
     for item_a in data_a:
         if item_a["title"] == item_b["Product"]:
             sum_value += item_a["price"] * item_b["Quantity"]
+            match_found = True
+
+    if not match_found:
+        print("Missing product:", item_b["Product"])
 
 print("Result:", sum_value)
