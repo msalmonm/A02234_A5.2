b0f773dfff3848503089713d668740b4050d2f4f
msalmonm <MMacal@hersheys.com>
2026-02-15 20:39:42 -0600

    fix: add argument length validation

---
+++ computeSales.py
@@ -0,0 +1,21 @@
+import json
+import sys
+
+if len(sys.argv) < 3:
+    print("Usage: python computeSales.py catalog.json sales.json")
+    sys.exit()
+
+file_a = sys.argv[1]
+file_b = sys.argv[2]
+
+data_a = json.load(open(file_a))
+data_b = json.load(open(file_b))
+
+sum_value = 0
+
+for item_b in data_b:
+    for item_a in data_a:
+        if item_a["title"] == item_b["Product"]:
+            sum_value += item_a["price"] * item_b["Quantity"]
+
+print("Result:", sum_value)
